e3c2315ccc8c95874e53272e6544918a793f78a5
ndr37 <ndr37@cornell.edu>
2019-06-03 12:49:35 -0400

    stage and daq control

---
+++ testDAQ.py
@@ -0,0 +1,23 @@
+from pymeasure.adapters import DAQmxAdapter
+from daedalus.custom_instruments import daedalusProjField, Keithley220
+import PyDAQmx as daqmx
+from daedalus.custom_instruments import senis3AxHallProbe
+
+daq = DAQmxAdapter('Dev2', ['ao0', 'ai1'])
+
+write_str = '/' + daq.resource_name + '/' + daq.channels[0]
+read_str = '/' + daq.resource_name + '/' + daq.channels[1]
+
+volts = 0.0
+
+task = daqmx.Task()
+task.CreateAOVoltageChan(write_str.encode(),"",-10.0,10.0,daqmx.DAQmx_Val_Volts,None)
+task.StartTask()
+task.WriteAnalogScalarF64(1,10.0,volts,None)
+task.StopTask()
+task.ClearTask()
+
+hall_probe = senis3AxHallProbe(DAQmxAdapter('Dev2', ['ai0','ai2','ai4']))
+print('x: %1.4f' % hall_probe.x_field)
+print('y: %1.4f' % hall_probe.y_field)
+print('z: %1.4f' % hall_probe.z_field)
---
+++ testStage.py
@@ -0,0 +1,5 @@
+from pymeasure.instruments.newport import ESP300
+a = ESP300(10)
+a.phi.position = 0.0
+a.x.position = 27.4
+a.y.position = 22.2
