7eae1673e236ea6f73a6377d65aea58ad8cceea1
Thow <tc676@cornell.edu>
2019-06-28 13:01:28 -0400

    field raster

--- testDAQ.py
+++ testDAQ.py
@@ -8,7 +8,7 @@ daq = DAQmxAdapter('Dev2', ['ao0', 'ai1'])
 write_str = '/' + daq.resource_name + '/' + daq.channels[0]
 read_str = '/' + daq.resource_name + '/' + daq.channels[1]
 
-volts = 5.0
+volts = 0.0
 
 task = daqmx.Task()
 task.CreateAOVoltageChan(write_str.encode(),"",-10.0,10.0,daqmx.DAQmx_Val_Volts,None)
@@ -18,6 +18,6 @@ task.StopTask()
 task.ClearTask()
 
 hall_probe = senis3AxHallProbe(DAQmxAdapter('Dev2', ['ai0','ai2','ai4']))
-print('x: %1.4f' % hall_probe.x_field)
-print('y: %1.4f' % hall_probe.y_field)
-print('z: %1.4f' % hall_probe.z_field)
+print('x: %1.7f' % hall_probe.x_field)
+print('y: %1.7f' % hall_probe.y_field)
+print('z: %1.7f' % hall_probe.z_field)
